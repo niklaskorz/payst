c5e524b072a19265c2ab719b7be99a4d4dd862dd
Niklas Korz <korz.niklask@gmail.com>
2013-02-03 10:25:53 +0100

    Removed setup files, created prepare_database.py

--- prepare_database.py
+++ prepare_database.py
@@ -1,6 +1,10 @@
-def setup_database(address):
-    from pycassa.system_manager import *
+from pycassa.system_manager import *
+
+def prepare_database(address="localhost:9160"):
     sys = SystemManager(address)
     sys.create_keyspace("Payst", SIMPLE_STRATEGY, {'replication_factor': '1'})
     sys.create_column_family("Payst", "Paste")
     sys.close()
+
+if __name__ == "__main__":
+    prepare_database()
--- setup.py
+++
@@ -1,4 +0,0 @@
-from payst import setup
-
-if __name__ == "__main__":
-    setup.setup_database("localhost:9160")
